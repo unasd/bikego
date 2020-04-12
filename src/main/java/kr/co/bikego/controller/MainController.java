99e4e42b5bd6e2af0c8195f62e5021791e9bc580
unasd <secondlinedragonfly@gmail.com>
2020-04-12 20:06:38 +0900

    패키지구조 생성

--- src/main/java/kr/co/bikego/controller/MainController.java
+++ src/main/java/kr/co/bikego/controller/MainController.java
@@ -1,6 +1,17 @@
 package kr.co.bikego.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
 public class MainController {
 
+    @GetMapping("/")
+    public String list(Model model){
+        return "/index";
+    }
 
 }
--- target/classes/static/js/crud.js
+++ target/classes/static/js/crud.js
@@ -1,1 +1,3 @@
-console.log(crudDto);
+$(document).ready(function(){
+
+});
