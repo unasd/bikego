4c95d6fe58c1cb184e1c90a59dbdee92cb31652e
unasd <secondlinedragonfly@gmail.com>
2020-05-19 06:19:12 +0900

    네이버 지도 적용

--- src/main/resources-local/static/js/asBoard.js
+++ src/main/resources-local/static/js/asBoard.js
@@ -1,3 +1,5 @@
+let map;
+
 function drawThumnail(input) {
     if (input.files && input.files[0]) {
         // 현재 썸네일 이미지가 몇개인지 가져옴
@@ -63,17 +65,79 @@ function crudSubmit(){
     $('#crudForm').submit();
 }
 
+var infowindow = new naver.maps.InfoWindow();
+function onSuccessGeolocation(position) {
+    var location = new naver.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
+
+    map.setCenter(location); // 얻은 좌표를 지도의 중심으로 설정합니다.
+    map.setZoom(18); // 지도의 줌 레벨을 변경합니다.
+}
+
+function onErrorGeolocation() {
+    var center = map.getCenter();
+
+    infowindow.setContent('<div style="padding:20px;">' +
+        '<h5 style="margin-bottom:5px;color:#f00;">Geolocation failed!</h5>'+ "latitude: "+ center.lat() +"<br />longitude: "+ center.lng() +'</div>');
+
+    infowindow.open(map, center);
+}
+
+function searchAddressToCoordinate(address) {
+  naver.maps.Service.geocode({
+    query: address
+  }, function(status, response) {
+    if (status === naver.maps.Service.Status.ERROR) {
+      if (!address) {
+        return alert('Geocode Error, Please check address');
+      }
+      return alert('Geocode Error, address:' + address);
+    }
+
+    if (response.v2.meta.totalCount === 0) {
+      return alert('No result.');
+    }
+
+    var htmlAddresses = [],
+      item = response.v2.addresses[0],
+      point = new naver.maps.Point(item.x, item.y);
+
+    if (item.roadAddress) {
+      htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
+    }
+
+    if (item.jibunAddress) {
+      htmlAddresses.push('[지번 주소] ' + item.jibunAddress);
+    }
+
+    if (item.englishAddress) {
+      htmlAddresses.push('[영문명 주소] ' + item.englishAddress);
+    }
+
+    infowindow.setContent([
+      '<div style="padding:10px;min-width:200px;line-height:150%;">',
+      '<h4 style="margin-top:5px;">검색 주소 : '+ address +'</h4><br />',
+      htmlAddresses.join('<br />'),
+      '</div>'
+    ].join('\n'));
+
+    map.setCenter(point);
+    infowindow.open(map, point);
+  });
+}
+
+
 $(document).ready(function(){
-    CKEDITOR.replace( 'contents',{
-        filebrowserUploadUrl: '/attach/ckImgUpload.do',
-        toolbarGroups: [
-            { name: 'editing',     groups: [ 'find', 'selection' ] },
-            { name: 'insert' },
-            { name: 'forms' },
-            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] }
-        ],
-        image_previewText: ' '
-    });
+//    CKEDITOR.replace( 'contents',{
+//        filebrowserUploadUrl: '/attach/ckImgUpload.do',
+//        toolbarGroups: [
+//            { name: 'editing',     groups: [ 'find', 'selection' ] },
+//            { name: 'insert' },
+//            { name: 'forms' },
+//            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] }
+//        ],
+//        image_previewText: ' '
+//    });
 
     CKEDITOR.on('dialogDefinition', function (ev) {
         var dialogName = ev.data.name;
@@ -115,5 +179,33 @@ $(document).ready(function(){
     var mapDiv = document.getElementById('map'); // 'map'으로 선언해도 동일
 
     //옵션 없이 지도 객체를 생성하면 서울 시청을 중심으로 하는 16 레벨의 지도가 생성됩니다.
-    var map = new naver.maps.Map(mapDiv);
+    map = new naver.maps.Map(mapDiv);
+
+        if (navigator.geolocation) {
+            /**
+             * navigator.geolocation 은 Chrome 50 버젼 이후로 HTTP 환경에서 사용이 Deprecate 되어 HTTPS 환경에서만 사용 가능 합니다.
+             * http://localhost 에서는 사용이 가능하며, 테스트 목적으로, Chrome 의 바로가기를 만들어서 아래와 같이 설정하면 접속은 가능합니다.
+             * chrome.exe --unsafely-treat-insecure-origin-as-secure="http://example.com"
+             */
+            navigator.geolocation.getCurrentPosition(onSuccessGeolocation, onErrorGeolocation);
+        } else {
+            var center = map.getCenter();
+            infowindow.setContent('<div style="padding:20px;"><h5 style="margin-bottom:5px;color:#f00;">Geolocation not supported</h5></div>');
+            infowindow.open(map, center);
+        }
+
+
+  $('#address').on('keydown', function(e) {
+    var keyCode = e.which;
+
+    if (keyCode === 13) { // Enter Key
+      searchAddressToCoordinate($('#address').val());
+    }
+  });
+
+  $('#submit').on('click', function(e) {
+    e.preventDefault();
+
+    searchAddressToCoordinate($('#address').val());
+  });
 });
